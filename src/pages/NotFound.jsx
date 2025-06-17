f75e7801ececf1aba53803c5eab411a111f3918f
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-06-18 00:16:20 +0100

    Add shadcn

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-        <section>
-            <div className="">
-                <h1>NotFound page</h1>
-            </div>
-        </section>
-    );
-}
